4f83d4d3c0af68be7b84abd2426c4c9f67127de4
Sugandha Amatya <sugandha.amatya@gmail.com>
2017-12-11 06:17:25 +0545

    removed chached dir and tftest

--- tftest.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-import os
-#export TF_CPP_MIN_LOG_LEVEL=2
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print(sess.run(hello))
-
